0d63f7625118c88d549c0ee78747c8321c58f89e
beicm <beicm2016@gmail.com>
2016-06-23 18:45:00 +0100

    Check another commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" / >
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<title>Backgroud Enginnering & Innovation</title>
+		<link rel="shortcut icon" href="assets/images/icons/favicon.png">
+		<link rel="stylesheet" type="text/css" href="vendor/Bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet/less" type="text/css" href="style.css">
+		<script type="text/javascript" src="core/less.min.js"></script>
+		
+	</head>
+	<!--[if lt IE 9]>
+	<script type="text/javascript" src="core/html5.js"></script>
+	<script type="text/javascript" src="core/respond.min.js"></script>
+	<![endif]-->
+<body>
+
+	<header id="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 block_bei">
+					
+						<a href="./"><img src="assets/images/img/logo_gr.png" alt="logo" title="logo_bei"></a>
+					
+				</div>
+			</div>
+		</div>
+	</header> <!-- END HEADER-->
+	
+	<div class="global_content">
+
+
+
+		<section id="description">
+	
+			<div class="soumission">
+				<div class="container">
+					<div class="row bei_contenu">
+						<div class="col-sm-6 col-sm-offset-3">
+
+							<div class="section_header">
+								<p ><span class="accent">Agence digital</span> <em>sp&eacute;cialis&eacute;e dans la création de 
+						 			sites web,ecommerce et applications.</em>
+						 		</p><br>
+						 		<p>Chaque projet n&eacute;cessite un accompagnement sur mesure et une expertise personnalisée.Souscrivez à la newsletter vous bénéficierez 
+									exclusivement d'<strong>1H</strong> de consultation 
+									gratuite.
+								</p>
+
+							</div>
+						</div>						
+					</div><!-- End row -->				
+				</div>
+			</div>
+			
+			<div class="solde">
+				<div class="container">									
+						<form action="#" role="form">
+							<div class="row">
+								<div class="col-sm-6 col-sm-offset-3 check">
+
+									<div class="col-sm-8">
+										<label for="contact_email" class="sr-only">Email</label>
+				 						<input type="text" id="contact_name" class="form-control" placeholder="Entrez votre adresse email" >
+									</div>
+									<div class="col-sm-4  col-sm-pull-1">
+										<div class="form-group">
+				 							<input type="button" class="btn btn-kapgon-sucess" value="S'inscrire">
+				 						</div>
+									</div>
+
+								</div>
+							</div>
+						</form>					
+				</div>
+				
+			</div>
+				
+
+		</section>		<!-- END SECTION WRITE US -->					
+
+	</div>
+
+	<script>window.jQuery || document.write('<script src="core/jquery.min.js">\x3C/script>')</script>
+	<script type="text/javascript" src="core/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="vendor/Bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="core/custom.js"></script>
+
+</body>
+</html>
