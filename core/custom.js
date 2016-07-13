78209da292bf7f361cd7a5682983f61db192555e
beicm <beicm2016@gmail.com>
2016-07-13 13:39:06 +0100

    Fisrt commit to Add direct link website

--- core/custom.js
+++ core/custom.js
@@ -1,8 +1,12 @@
 'use strict';
 
-
 $(document).ready( function(){
 
+	$( 'button.btn_go' ).click(  function(){
+		
+		document.location.href="site/";
+
+	} );
 	
 } );
 
---
+++ site/core/custom.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// jQuery( document ).ready( function(){
+// 	// console.log( 'jquery is ready' );
+
+// 	jQuery( window ).scroll( function(){
+// 		// console.log(jQuery( this ).scrollTop());
+// 		var $_test = jQuery( 'section[data-type="background"]' );
+// 		console.log( jQuery( this ).scrollTop() );
+// 	});
+// });
+
+jQuery( function(){
+	//cache window object
+	var mnEcran = jQuery( window );
+	//recuperation des différentes attribut crées
+
+	jQuery( 'section[data-type="background"]' ).each( function(){
+		//recuperatin de la section à annuler 
+		var imgbg_ = jQuery( this );
+		// console.log( imgbg_.data( 'speed' ) ); 
+		// Donner les instructions d'animations
+
+		jQuery( window ).scroll( function() {
+			var yPos = - ( mnEcran.scrollTop() / imgbg_.data( 'speed' ) );
+			// console.log( yPos );
+			var Cord_ = ' 50% ' + yPos + ' px ';
+			jQuery( imgbg_ ).css( {backgroundPosition: Cord_} );
+
+		} );
+
+	} );
+
+	
+});
+
+$(document).ready( function(){
+
+
+	var repere_menu = 45;
+
+	$( '#bei_carousel' ).carousel({
+		interval:5000,
+		cycle:true
+	});
+
+	$( '.nav_bei li > a' ).click( function(evt){
+		evt.preventDefault();
+		$( '.nav_bei li a' ).removeClass( 'actived' );
+		$( this ).addClass( 'actived' );
+		
+	} );
+
+	// determine height position of my cursor
+
+	$( window ).scroll(  function(){
+		console.log( $( this ).scrollTop() );
+	} );
+
+	$("a[href^='#']").click(function(e){
+
+		var pos = $(this.hash).offset().top - repere_menu;
+		$('body, html').stop().animate({ scrollTop:pos }, 1500);
+		e.preventDefault();
+
+	});
+
+} )
+
